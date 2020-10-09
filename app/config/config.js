699608625e1e245dec241aaca3033f8938527ff4
jackie <mvpbremah@yahoo.com>
2020-10-09 03:18:11 -0400

    commit

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root12345",
+    "password": "Bremen12345$",
     "database": "battler_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
